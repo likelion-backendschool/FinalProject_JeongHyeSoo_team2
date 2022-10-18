2579379eb5e915d46c844393a7aaf987459b8361
JeongHyeSoo <nailonly@naver.com>
2022-10-19 02:35:06 +0900

    setting

---
+++ 1Week_Mission/src/main/java/com/ll/project/Week_Mission/WeekMissionApplication.java
@@ -0,0 +1,13 @@
+package com.ll.project.Week_Mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekMissionApplication.class, args);
+	}
+
+}
---
+++ 1Week_Mission/src/test/java/com/ll/project/Week_Mission/WeekMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.project.Week_Mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
